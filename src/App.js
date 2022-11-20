6958696639b2be14846ee20df1f8e4f79dc42bff
Chani Wachsstock <chaniwachsstock@gmail.com>
2022-11-20 17:44:47 -0500

    added console.log

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from "./logo.svg";
 import "./output.css";
 import { SelectSection } from "./components/SelectSection";
 import { MenuSection } from "./components/MenuSection";
-import { CookingSection } from "./components/CookingSection";
-import { useState } from "react";
+// import { CookingSection } from "./components/CookingSection";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [menus, setMenus] = useState([{}]);
+  const [menus, setMenus] = useState([{ category: "Chicken" }]);
+
+  useEffect(() => {
+    console.log(menus);
+  }, [menus]);
+
   return (
     <div className="App">
       <SelectSection menus={menus} setMenus={setMenus} />
