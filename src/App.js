bdead603237fa50c034d1a74b181bb367b924c95
LioraW <liorawachsstock@gmail.com>
2022-11-20 15:19:35 -0500

    api start...:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Hi } from './components/hi';
+import React from 'react';
+import DadJokes from './components/DadJokes';
 
 function App() {
   return (
--- src/api/getRecipe.js
+++ src/api/getRecipe.js
@@ -1,16 +1,38 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+import { corsSimpleRequests } from '../cors';
 
-const dom = new JSDOM(``, {
-    url: "https://allrecipes.com/recipe/282681/lace-cookies-florentine-cookies/",
-    referrer: "https://allrecipes.com/recipe/282681/lace-cookies-florentine-cookies/",
+const axios = require('axios').default;
 
-    contentType: "text/html",
-    includeNodeLocations: true,
-    storageQuota: 10000000
-  });
+export const getRecipe = async () => {
+    
 
-export const getRecipe = () => {
-    return JSON.stringify(dom)
+const url = "https://www.allrecipes.com/recipe/229932/simple-pumpkin-pie/";
+    const recipeUrl = 'https://tasty.p.rapidapi.com/recipes/auto-complete'
+    const kosherUrl = 'https://www.kosher.com/recipe/5-minute-yummy-chicken-2701'
+
+    
+   
+
+        
+        const options = {
+            method: 'GET',
+            url: 'https://tasty.p.rapidapi.com/recipes/auto-complete',
+            params: {prefix: 'chicken soup'},
+            headers: {
+              'X-RapidAPI-Key': 'b11b26eb74mshc1689e69717c923p108c44jsn489151bf0397',
+              'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+            }
+          };
+          
+          await axios.request(options).then(function (response) {
+              console.log(response.data);
+              //setRecipe(JSON.stringify(response.data.results[0].display))
+              return JSON.stringify(response.data.results[0].display)
+          }).catch(function (error) {
+              console.error(error);
+              return ''
+          });
+
+
+
 }
 
