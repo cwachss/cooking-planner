464f4ec364e38d53ceac0d0c645d50cb995b27b6
Esther Rosenberg <erosenberg@torahanytime.com>
2022-11-20 19:24:21 -0500

    fix: overflow

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -14,7 +14,7 @@ export const RecipeCard = ({ recipe }) => {
       </button>
       {open && (
         <div className="absolute z-30 inset-0 bg-gray-100/70 grid place-content-center">
-          <div className="rounded bg-white p-6 w-[50vw] h-[90vh]">
+          <div className="rounded bg-white p-6 w-[50vw] h-[90vh] overflow-y-auto">
             <button
               className="float-right text-2xl leading-0 font-bold rounded-full bg-gray-100 w-10 h-10 grid place-content-center"
               onClick={() => setOpen(false)}
